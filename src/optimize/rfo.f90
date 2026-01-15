1034af3b863aa725edd114b8f9df581cf86ceb3b
LukasRindt <ff256@stud.uni-heidelberg.de>
2026-01-15 13:20:34 +0100

    minor printout change

--- src/optimize/rfo.f90
+++ src/optimize/rfo.f90
@@ -373,12 +373,7 @@ contains  !> MODULE PROCEDURES START HERE
           dsnrm = sqrt(ddot(nvar1,Uaug,1,Uaug,1))
           Uaug = Uaug/dsnrm
         else
-          write(stdout,*) "Uaug before solver", Uaug
-          write(stdout,*) "Uaug relevant value before solver", abs(Uaug(nvar1,1))
           call solver_dspevx(nvar1,r4dum,Aaug,Uaug,eaug,fail)
-          write(stdout,*) "Uaug after solver", Uaug
-          write(stdout,*) "Uaug relevant value after solver", abs(Uaug(nvar1,1))
-          write(stdout,*) "Failstatement", fail
         endif
       else
         !>--- steepest decent guess for displacement
