6d4d85867c7c14a877d15cb24d9f62fdf3022800
Philipp Pracht <research@philipp-pracht.de>
2026-03-25 18:37:40 +0100

    update dftd4 subproject across builds

--- config/modules/Finddftd4.cmake
+++ config/modules/Finddftd4.cmake
@@ -17,7 +17,7 @@
 set(_lib "dftd4")
 set(_pkg "DFTD4")
 set(_url "https://github.com/dftd4/dftd4")
-set(_branch "v3.7.0")
+set(_branch "v4.0.0")
 
 if(NOT DEFINED "${_pkg}_FIND_METHOD")
   set("${_pkg}_FIND_METHOD" "subproject" "cmake" "fetch" "pkgconf")
