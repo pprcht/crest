ae8f66979df9fca1c35bea024f9bfb69f686c5ea
Philipp Pracht <research@philipp-pracht.de>
2026-04-24 13:41:17 +0200

    Update in preparation for tblite gxtb build

--- src/calculator/tblite_api.F90
+++ src/calculator/tblite_api.F90
@@ -125,7 +125,9 @@ contains  !> MODULE PROCEDURES START HERE
 !* passed between the CREST calculators and this module
 !*****************************************************************
 #ifdef WITH_TBLITE
+#ifdef WITH_GXTB
     use multicharge,only:get_charges
+#endif
 #endif
     implicit none
     type(coord),intent(in)  :: mol
@@ -212,11 +214,13 @@ contains  !> MODULE PROCEDURES START HERE
 
 !>--- for methods with an auxiliary charge model (e.g., gxTB), pre-allocate wfn_aux.
 !>--- Charges are updated at each singlepoint call (geometry-dependent).
+#ifdef WITH_GXTB
     if (allocated(tblite%calc%charge_model)) then
       if (allocated(tblite%wfn_aux)) deallocate(tblite%wfn_aux)
       allocate(tblite%wfn_aux)
       call new_wavefunction(tblite%wfn_aux,mctcmol%nat,tblite%calc%bas%nsh,0,1,0.0_wp,.true.)
     end if
+#endif
 
 #else /* WITH_TBLITE */
     write (stdout,*) 'Error: Compiled without tblite support!'
@@ -384,7 +388,9 @@ contains  !> MODULE PROCEDURES START HERE
 !* The tblite object must be set up at this point
 !**************************************************
 #ifdef WITH_TBLITE
+#ifdef WITH_GXTB
     use multicharge,only:get_charges
+#endif
 #endif
     implicit none
     type(coord),intent(in)   :: mol
@@ -415,6 +421,7 @@ contains  !> MODULE PROCEDURES START HERE
     call tblite_mol2mol(mol,chrg,uhf,mctcmol)
 
 !>--- update geometry-dependent EEQ-BC charges in wfn_aux (allocated once in tblite_setup)
+#ifdef WITH_GXTB
     if (allocated(tblite%wfn_aux)) then
       call get_charges(tblite%calc%charge_model,mctcmol,error,tblite%wfn_aux%qat(:,1), &
       &                dqdr=tblite%wfn_aux%dqatdr(:,:,:,1),dqdL=tblite%wfn_aux%dqatdL(:,:,:,1))
@@ -424,10 +431,12 @@ contains  !> MODULE PROCEDURES START HERE
         return
       end if
     end if
+#endif
 
 !>--- call the singlepoint routine
     select case (tblite%lvl)
     case default
+#ifdef WITH_GXTB
       if (allocated(tblite%wfn_aux)) then
         call xtb_singlepoint(tblite%ctx,mctcmol,tblite%calc,tblite%wfn,tblite%accuracy, &
         &                    energy,gradient,sigma,verbosity,results=tblite%res,wfn_aux=tblite%wfn_aux)
@@ -435,6 +444,10 @@ contains  !> MODULE PROCEDURES START HERE
         call xtb_singlepoint(tblite%ctx,mctcmol,tblite%calc,tblite%wfn,tblite%accuracy, &
         &                    energy,gradient,sigma,verbosity,results=tblite%res)
       end if
+#else
+        call xtb_singlepoint(tblite%ctx,mctcmol,tblite%calc,tblite%wfn,tblite%accuracy, &
+        &                    energy,gradient,sigma,verbosity,results=tblite%res)
+#endif
     case (xtblvl%ceh)
       call ceh_singlepoint(tblite%ctx,tblite%calc,mctcmol,tblite%wfn, &
       &              tblite%accuracy,verbosity)
@@ -501,7 +514,7 @@ contains  !> MODULE PROCEDURES START HERE
     logical,intent(in) :: saveint
     real(wp),intent(in) :: accuracy
 #ifdef WITH_TBLITE
-    tblite%calc%iterator%max_iter = maxscc
+    tblite%calc%max_iter = maxscc
     tblite%calc%save_integrals = (rdwbo.or.saveint)
     tblite%accuracy = accuracy
 #endif
